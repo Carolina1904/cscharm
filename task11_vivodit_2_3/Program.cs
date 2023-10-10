38084be68f9988ed53a0009d3a5cf948c1225237
Carolina <trifoncarolina17@gmail.com>
2023-10-10 22:11:20 +0300

    deleted

---
+++ task11_vivodit_2_3/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Напишите программу которая выводит случайное трезназное число и удаляет вторую цифру этого числа
+456->46
+782->72
+*/
+
+int number = new Random().Next(100,1000);
+System.Console.WriteLine(number);
+int first = number / 100;
+int last = number % 10;
+System.Console.Write("answer = " + first + last);
