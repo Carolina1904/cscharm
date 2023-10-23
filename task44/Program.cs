757b7bd69c6a1a0e2e8c54ceea23e2fed346fc94
Carolina <trifoncarolina17@gmail.com>
2023-10-23 12:56:25 +0300

    23.10

---
+++ task44/Program.cs
@@ -0,0 +1,36 @@
+﻿//
+
+Console.Write("Введите количество чисел Фибоначчи: ");
+int N = int.Parse(Console.ReadLine());
+
+if (N < 1)
+{
+    Console.WriteLine("Пожалуйста, введите положительное число.");
+    return;
+}
+
+int first = 0;
+int second = 1;
+
+Console.Write("Первые " + N + " чисел Фибоначчи: ");
+
+if (N >= 1)
+{
+    Console.Write(first);
+}
+
+if (N >= 2)
+{
+    Console.Write(", " + second);
+}
+
+for (int i = 3; i <= N; i++)
+{
+    int next = first + second;
+    Console.Write(", " + next);
+    first = second;
+    second = next;
+}
+
+Console.WriteLine();
+
