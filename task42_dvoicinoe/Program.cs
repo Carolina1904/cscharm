92cc9fdb673591787e8867ea5c3aa90d60fe646f
Carolina <trifoncarolina17@gmail.com>
2023-10-23 12:56:47 +0300

    23.10

---
+++ task42_dvoicinoe/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 42: Напишите программу, которая будет преобразовывать
+десятичное число в двоичное.
+*/
+/*Console.Write("Введите десятичное число: ");
+int decimalNumber = int.Parse(Console.ReadLine());
+
+string binaryNumber = DecimalToBinary(decimalNumber);
+
+Console.WriteLine($"Двоичное представление: {binaryNumber}");
+
+
+string DecimalToBinary(int decimalNumber)
+{
+    return Convert.ToString(decimalNumber, 2);
+}*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ToBool( int number)
+{
+    int result= 0;
+    int count = 1;
+    while (number>0)
+    {
+        result=number%2*count+result;
+        number=number/2;
+        count=count*10;
+    }
+return result;
+}
+int Number = ToBool(number);
+Console.Write($"Двоичное представление {number} ->  " + Number );
