c1b45dddfed2f0ae7e74ae2b766c5e1037068662
Carolina <trifoncarolina17@gmail.com>
2023-10-24 13:37:46 +0300

    24.10

---
+++ task41_count_num/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Write("Введите количество чисел (M): ");
+if (int.TryParse(Console.ReadLine(), out int M) && M > 0)
+{
+    double[] numbers = new double[M]; // Создаем массив для хранения чисел
+
+    for (int i = 0; i < M; i++)
+    {
+        Console.Write($"Введите число {i + 1}: ");
+        if (double.TryParse(Console.ReadLine(), out double number))
+        {
+            numbers[i] = number;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Введите число заново.");
+            i--; // Повторить ввод для данной итерации
+        }
+    }
+
+    int count = 0; // Переменная для подсчета чисел больше 0
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        double number = numbers[i];
+
+        if (number > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Количество чисел больше 0: {count}");
+}
+else
+{
+    Console.WriteLine("Некорректное количество чисел. Пожалуйста, введите положительное целое число.");
+}
+
