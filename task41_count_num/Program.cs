9af900ccf4eb2385f8de5ce0cdb9fc806a217dd3
Carolina <trifoncarolina17@gmail.com>
2023-10-24 14:09:50 +0300

    24.10

--- task41_count_num/Program.cs
+++ task41_count_num/Program.cs
@@ -41,3 +41,12 @@ else
     Console.WriteLine("Некорректное количество чисел. Пожалуйста, введите положительное целое число.");
 }
 
+/*int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+
+    return result;
+}*/
+
